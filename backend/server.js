3ce83e8c14b69fef8827269b086e7d99355c989e
Joey Dang <jodang@ucsd.edu>
2021-02-01 14:57:39 -0800

    Handle CORS

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const food = require('./routes/food');
 const recipes = require('./routes/recipes');
 const user = require('./routes/user');
-const food = require('./routes/food');
 const config = require('./config');
 
 mongoose.connect(config.mongodb_uri, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => {
+    app.use(cors());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     
