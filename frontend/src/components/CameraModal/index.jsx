c0eff86ae939fea954a18dc50e8331bc443db346
Joey Dang <jodang@ucsd.edu>
2021-02-25 18:55:11 -0800

    Add Camera Scan

--- frontend/src/components/CameraModal/index.jsx
+++ frontend/src/components/CameraModal/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Modal } from 'antd';
+import Webcam from "react-webcam";
 
 import './style.scss';
 
 const CameraModal = (props) => {
+
+  const [imageSrc, setImageSrc] = useState('');
+  const webcamRef = useRef(null);
 
   const closeModal = () => {
     props.closeModal();
@@ -12,14 +16,35 @@ const CameraModal = (props) => {
   return (
     <div className="camera-modal-container">
       <Modal
+        closable={false}
         footer={null}
         onCancel={closeModal}
         onOk={closeModal}
         visible={props.isModalVisible}
-        width="90%"
+        width="100%"
       >
         <div className="camera-modal">
-          Hello World
+          <h1>Item</h1>
+          <Webcam
+              ref={webcamRef}
+              className="camera"
+              screenshotQuality={1}
+              height={720}
+              width={1624}
+            />
+          { imageSrc !== '' && 
+            <img alt="screeenshot" className="screenshot" src={imageSrc}></img>
+          }
+          <button
+            id="scan"
+            onClick={() => {
+              const base64 = webcamRef.current.getScreenshot();
+              console.log(base64);
+              setImageSrc(base64);
+            }}
+          >
+            Scan
+          </button>
         </div>
       </Modal>
     </div>
