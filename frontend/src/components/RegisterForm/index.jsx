841fc2fb31135d8180113b00b0cdc26d9bbf245c
Joey Dang <jodang@ucsd.edu>
2021-02-28 18:12:11 -0800

    Update Login form

--- frontend/src/components/RegisterForm/index.jsx
+++ frontend/src/components/RegisterForm/index.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
+import envelope from '../../assets/envelope.svg'
+import lock from '../../assets/lock.svg'
 
 import "./style.scss";
 
-const RegisterForm = () => {
+const RegisterForm = (props) => {
   return (
-    <div className="register">
-      <Formik>
-        <Form className="register-form">
-          <Field className="form" type="text" placeholder="Username" />
-          <Field className="form" type="password" placeholder="Password" />
-        </Form>
-    </Formik>
-    </div>
+    <Formik>
+      <Form className="register-form">
+        <div className="form">
+          <Field className="username" type="text" placeholder="Email" />
+          <img alt="email" className="email" src={envelope}/>
+        </div>
+        <div className="form">
+          <Field className="password" type="password" placeholder={props.password} />
+          <img alt="lock" className="lock" src={lock}/>
+        </div>
+      </Form>
+      </Formik>
   );
 };
 
