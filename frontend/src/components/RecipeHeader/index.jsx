e6f9eade577e432e3077ed835f66c8e12abba9bb
Joey Dang <jodang@ucsd.edu>
2021-03-13 18:11:32 -0800

    Fix bugs

--- frontend/src/components/RecipeHeader/index.jsx
+++ frontend/src/components/RecipeHeader/index.jsx
@@ -10,13 +10,13 @@ const RecipeHeader = (props) => (
   <div className="recipe-header">
     <Link to="/home">
         <img alt="back" className="back" src={back}/>
-      </Link>
-      <div className="user">
-        <img alt="user" src={user}/>
-        <span>{props.name}</span>
-      </div> 
-      <h1><span className="underline">Reco</span>mmended</h1>
-      <h2 className="description">recipes that include more than one ingredients you entered</h2>
+    </Link>
+    <div className="user">
+      <img alt="user" src={user}/>
+      <span>{props.name}</span>
+    </div> 
+    <h1><span className="underline">Reco</span>mmended</h1>
+    <h2 className="description">recipes that include more than one ingredients you entered</h2>
   </div>
 )
 
--- frontend/src/components/RecipeTab/index.jsx
+++ frontend/src/components/RecipeTab/index.jsx
@@ -6,11 +6,11 @@ const RecipeTab = (props) => {
   return (
     <div className="recipe-tab">
       {props.instructions.map((instruction) => (
-      <div className="recipe-list">
-        <h1 className="step-number">{instruction.number}</h1>
-        <p>{instruction.step}</p>
-      </div>
-          ))}
+        <div className="recipe-list">
+          <h1 className="step-number">{instruction.number}</h1>
+          <p>{instruction.step}</p>
+        </div>
+      ))}
     </div>
   )
 }
